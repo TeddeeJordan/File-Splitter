1b1426cfeafe15c6f0b53ca9792c2ec229a91ecc
Teddee Jordan <43998351+TeddeeJordan@users.noreply.github.com>
2019-10-29 15:06:52 -0500

    Update concurrentfilesplitter.ps1

--- concurrentfilesplitter.ps1
+++ concurrentfilesplitter.ps1
@@ -1,20 +1,20 @@
 <#
-This script takes the large televox.txt file sent by Partners each morning.
+This script takes the large filename.txt file sent by Client each morning.
 Instead of cutlocs, we use a csv to split it into smaller files by acct ID.
 These files can then be parsed concurrently by our system for speed.
 #>
 
 
 #File Parameters
-$diplomat_file = '\\tvincoming\sshroot\tst016\televox.txt'
+$vendor_file = '\\place\sshroot\tst016\filename.txt'
 
 #Read csv of locs/accts
 $acctsHeader = "EpicLocID","Acct"
-$acct = Import-Csv -Path "\\tvincoming\sshroot\tst016\partners_accts.csv" -header $acctsHeader
+$acct = Import-Csv -Path "\\place\sshroot\tst016\accts.csv" -header $acctsHeader
 
 #Read Appts File
 $fileHeaders = "A","CSN","F","Date","Time","Duration","Proc","1","2","3","4","5","6","Loc","Doc","ZNum","7","Last","First","Primary","Lang","Cell","Birth","Record","Pref","8","9"
-$file = Import-Csv -Path $diplomat_file -header $fileHeaders
+$file = Import-Csv -Path $vendor_file -header $fileHeaders
 
 
 #Append records to the prerequesite files
@@ -24,7 +24,7 @@ ForEach($files in $file){
         #Write-Host $acct_check.EpicLocID
         if($files.Loc -eq $accts.EpicLocID){
             $record = $files | Convertto-Csv -NoType | Select-Object -Skip 1 | %{$_-replace '"',""}
-            $addedFile = '\\tvincoming\sshroot\tst016\' + $accts.Acct + '_televox.txt'
+            $addedFile = '\\place\sshroot\tst016\' + $accts.Acct + '_filename.txt'
             Add-Content $addedFile $record
          }
    }
